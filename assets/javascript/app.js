57ac9867c8669673cfd62fe518c35bf6532b835b
J Lew <momentary232@gmail.com>
2019-03-25 01:36:33 -0400

    add html

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -9,26 +9,34 @@
   };
   firebase.initializeApp(config);
 var database = firebase.database();
-var provider = new firebase.auth.GoogleAuthProvider();
+//var provider = new firebase.auth.GoogleAuthProvider();
 //$("#submit").on("click", function(event) {
-    event.preventDefault();
-    firebase.auth().signInWithRedirect(provider);
-    firebase.auth().getRedirectResult().then(function(result) {
-        if (result.credential) {
-          // This gives you a Google Access Token. You can use it to access the Google API.
-          var token = result.credential.accessToken;
-          // ...
-        }
-        // The signed-in user info.
-        var user = result.user;
-      }).catch(function(error) {
-        // Handle Errors here.
-        var errorCode = error.code;
-        var errorMessage = error.message;
-        // The email of the user's account used.
-        var email = error.email;
-        // The firebase.auth.AuthCredential type that was used.
-        var credential = error.credential;
-        // ...
-      });
+//    event.preventDefault();
+    // firebase.auth().signInWithRedirect(provider);
+    // firebase.auth().getRedirectResult().then(function(result) {
+    //     if (result.credential) {
+    //       // This gives you a Google Access Token. You can use it to access the Google API.
+    //       var token = result.credential.accessToken;
+    //       // ...
+    //     }
+    //     // The signed-in user info.
+    //     var user = result.user;
+    //   }).catch(function(error) {
+    //     // Handle Errors here.
+    //     var errorCode = error.code;
+    //     var errorMessage = error.message;
+    //     // The email of the user's account used.
+    //     var email = error.email;
+    //     // The firebase.auth.AuthCredential type that was used.
+    //     var credential = error.credential;
+    //     // ...
+    //   });
    // });
+
+   var tName;
+   var tDest;
+   var tFreq;
+   var tFirst;
+
+   
+
