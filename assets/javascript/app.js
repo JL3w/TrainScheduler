5344f008e2c806fac900dc9d6806251d38448396
J Lew <momentary232@gmail.com>
2019-03-24 20:53:55 -0400

    initial files and directories

---
+++ assets/javascript/app.js
@@ -0,0 +1,31 @@
+var config = {
+    apiKey: "AIzaSyD2IpQtH4Ip9Xh2c3xuSxd3C24xFIS2NnA",
+    authDomain: "train-app-1171a.firebaseapp.com",
+    databaseURL: "https://train-app-1171a.firebaseio.com",
+    projectId: "train-app-1171a",
+    storageBucket: "train-app-1171a.appspot.com",
+    messagingSenderId: "1016489823423"
+  };
+  firebase.initializeApp(config);
+var database = firebase.database();
+var provider = new firebase.auth.GoogleAuthProvider();
+
+firebase.auth().signInWithPopup(provider).then(function(result) {
+    // This gives you a Google Access Token. You can use it to access the Google API.
+    var token = result.credential.accessToken;
+    // The signed-in user info.
+    var user = result.user;
+    // ...
+  }).catch(function(error) {
+    // Handle Errors here.
+    var errorCode = error.code;
+    var errorMessage = error.message;
+    // The email of the user's account used.
+    var email = error.email;
+    // The firebase.auth.AuthCredential type that was used.
+    var credential = error.credential;
+    // ...
+  });
+
+
+
